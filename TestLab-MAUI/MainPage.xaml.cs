4f80fdf876668c7251e95e6a694711edd072de4f
N.DZL <cxnt48@gmail.com>
2024-02-27 09:45:40 +0100

    Add project files.

---
+++ TestLab-MAUI/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using CommunityToolkit.Mvvm.Messaging;
+
+namespace OEMInfo_MAUI_Sample
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            WeakReferenceMessenger.Default.Register<string>(this, (r, m) =>
+            {
+                    MainThread.BeginInvokeOnMainThread(() => { lbDisplayDeviceId.Text += "\n" + m; });
+            });
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+
+            WeakReferenceMessenger.Default.Send("#ABC");
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+
+}
